d5e9ffea92d0b28c6c53ed2457a503950682ae2d
satviksriv <srivastavasatvik22@gmail.com>
2022-02-11 03:58:02 +0530

    Login endpoint and acess tokens

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-//* User model
-const User = mongoose.model("User", {
+//* User schema
+const userSchema = mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -10,6 +12,7 @@ const User = mongoose.model("User", {
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -38,7 +41,53 @@ const User = mongoose.model("User", {
                 throw new Error(`Password cannot contain "password"`);
             }
         },
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, "thisismynewcourse");
+
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+
+    return token;
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error("Unable to login");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        throw new Error("Unable to login");
+    }
+
+    return user;
+}
+
+//* Hash the plain text password before saving
+userSchema.pre("save", async function (next) {
+    const user = this;
+
+    if (user.isModified("password")) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    next();
+})
+
+//* User model
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
