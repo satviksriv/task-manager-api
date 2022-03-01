721a46191568ef5ea4a0369445a0c695cf05e4f0
satviksriv <srivastavasatvik22@gmail.com>
2022-03-02 02:06:05 +0530

    Added more tasks tests

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+const { resource } = require("../src/app");
 const app = require("../src/app");
 const Task = require("../src/models/task");
 const {
@@ -46,13 +47,59 @@ test("Should not delete unauthorized tasks", async () => {
     expect(task).not.toBeNull();
 });
 
+test("Should not create task with invalid description/completed", async () => {
+    await request(app)
+        .post("/tasks")
+        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: 12321,
+            completed: "not completed"
+        })
+        .expect(400);
+});
+
+test("Should not update task with invalid description/completed", async () => {
+    await request(app)
+        .patch(`/tasks/${taskOne._id}`)
+        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: 12321,
+            completed: "not completed"
+        })
+        .expect(400);
+});
+
+test("Should delete user task", async () => {
+    const response = await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200);
+    const task = await Task.findById(response.body._id)
+    expect(task).toBeNull();
+});
+
+test("Should not delete task if unauthenticated", async () => {
+    await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set("Authorization", `Bearer invalidWebToken`)
+        .send()
+        .expect(401);
+});
+
+test("Should not update other users task", async () => {
+    await request(app)
+        .patch(`/tasks/${taskOne._id}`)
+        .set("Authorization", `Bearer ${userTwo.tokens[0].token}`)
+        .send({
+            description: "Updated description",
+            completed: true
+        })
+        .expect(404);
+});
+
 // Task Test Ideas
 //
-// Should not create task with invalid description/completed
-// Should not update task with invalid description/completed
-// Should delete user task
-// Should not delete task if unauthenticated
-// Should not update other users task
 // Should fetch user task by id
 // Should not fetch user task by id if unauthenticated
 // Should not fetch other users task by id
